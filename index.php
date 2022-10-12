ced26df2290ee6dd1aeaa8ed26ea667f96e8b5cd
DubanSteven97 <105088154+DubanSteven97@users.noreply.github.com>
2022-10-11 22:13:15 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Proyecto
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" href="js/bootstrap.min.js"></script>
+    <script type="text/javascript" href="js/jquery-3.6.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div class="loader"></div>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Tree sort</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Generalidades">Generalidades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Funcionamiento">Funcionamiento</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Demostracion">Demostración</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 id="Generalidades">Generalidades</h1>
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitis</p>
+                </div>
+            </div>
+            <div class="row">
+                <h2 id="Funcionamiento">Funcionamiento</h2>
+                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitis.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. Fugit, debitisLorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+                        perspiciatis
+                        ipsa delectus quos
+                        commodi eius esse sint atque, nemo repudiandae distinctio cupiditate veritatis asperiores odit
+                        dicta
+                        quaerat vero. </p>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/n2MLjGeK7qA"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h2 id="Demostracion">Demostración</h2><br>
+                    <form class="row g-3" name="formulario" action="php/generacion.php" enctype="multipart/form-data"
+                        method="post" enctype="multipart/form-data" required>
+                        <div class="col-12">
+                            <select class="form-select" aria-label="Default select example" id="algoritmo"
+                                name="algoritmo">
+                                <option disabled>Algoritmos de organización</option>
+                                <option value="Tree sort">Tree sort</option>
+                                <option value="Otro1">Otro 1</option>
+                                <option value="Otro2">Otro 1 </option>
+                            </select>
+                        </div>
+                        <div class="col-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                    id="flexRadioDefault1" require value="aleatorios">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    Datos aleatorios
+                                </label>
+                                <div class="tipo-dato">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault_tipo"
+                                            id="flexRadioDefault1_numericos" require value="numerico" disabled>
+                                        <label class="form-check-label" for="flexRadioDefault1_numericos">
+                                            Datos numéricos
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault_tipo"
+                                            id="flexRadioDefault2_letras" required value="letras" disabled>
+                                        <label class="form-check-label" for="flexRadioDefault2_letras">
+                                            Datos texto
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault"
+                                    id="flexRadioDefault2" required value="archivo">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    Datos archivo
+                                </label>
+                            </div>
+
+                            <div class="adjunto">
+                                <input type="file" name="adjunto[]" class="form-control" aria-label="file example"
+                                    id="adjunto[]" accept=".txt" multiple="" disabled>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="inicio">
+                                <select class="form-select" multiple aria-label="multiple select example"
+                                    id="numeros_inicio" name="numeros_inicio" required disabled>
+                                    <option disabled>Números de datos de inicio</option>
+                                    <option value="1000" selected>1000</option>
+                                    <option value="2000">2000</option>
+                                    <option value="3000">3000</option>
+                                    <option value="Otro">Otro</option>
+                                </select>
+                            </div>
+
+                            <div class="otro1">
+                                <label for="otro1" class="form-label">Otro:</label>
+                                <input type="number" class="form-control" id="otro1" name="otro1" min="1"
+                                    pattern="^[0-9]+" aria-describedby="emailHelp" required disabled>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="iteraciones">
+                                <label for="Iteraciones" class="form-label">Iteraciones</label>
+                                <input type=" number" class="form-control" id="Iteraciones" name="Iteraciones"
+                                    aria-describedby="emailHelp" min="1" pattern="^[0-9]+" required disabled>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="avance">
+                                <select class="form-select" multiple aria-label="multiple select example"
+                                    id="avance_iteracion" name="avance_iteracion" required disabled>
+                                    <option disabled>Avance entre iteración</option>
+                                    <option value="500" selected> 500</option>
+                                    <option value="1000">1000</option>
+                                    <option value="2000">2000</option>
+                                    <option value="Otro">Otro</option>
+                                </select>
+                            </div>
+                            <div class="otro2">
+                                <div>
+                                    <label for="otro2" class="form-label">Otro:</label>
+                                    <input type=" number" class="form-control" id="otro2" name="otro2"
+                                        aria-describedby="emailHelp" min="1" pattern="^[0-9]+" required disabled>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <center> <button type="submit" class="btn btn-primary" id="enviar"
+                                    name="enviar">Enviar</button></center>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div><br>
+    </main>
+
+    <script>
+    var adjunto = document.getElementById('adjunto[]');
+    var dato1 = document.getElementById('flexRadioDefault2_letras');
+    var dato2 = document.getElementById('flexRadioDefault1_numericos');
+    var otro1 = document.getElementById('otro1');
+    var otro2 = document.getElementById('otro2');
+    var inicio = document.getElementById('numeros_inicio');
+    var iteraciones = document.getElementById('Iteraciones');
+    var avance = document.getElementById('avance_iteracion');
+    var enviar = document.getElementById('enviar');
+
+
+    // evento para el input radio del "si"
+    document.getElementById('flexRadioDefault2').addEventListener('click', function(e) {
+        console.log('Vamos a habilitar el campo para el adjunto');
+        adjunto.disabled = false;
+        dato1.disabled = true;
+        dato2.disabled = true;
+        iteraciones.disabled = true;
+        avance.disabled = true;
+    });
+
+    // evento para el input radio del "no"
+    document.getElementById('flexRadioDefault1').addEventListener('click', function(e) {
+        console.log('Vamos a deshabilitar el campo para el adjunto');
+        adjunto.disabled = true;
+        dato1.disabled = false;
+        dato2.disabled = false;
+        iteraciones.disabled = false;
+        avance.disabled = false;
+    });
+
+    // evento para habilitar campo otro1
+    document.getElementById('numeros_inicio').addEventListener('click', function(e) {
+        if (document.formulario.numeros_inicio.selectedIndex == 4) {
+            console.log('Vamos a habilitar el campo para el adjunto');
+            otro1.disabled = false;
+        }
+
+    });
+
+    //evento para desabilitar campo otro1
+    document.getElementById('numeros_inicio').addEventListener('click', function(e) {
+        if (document.formulario.numeros_inicio.selectedIndex != 4) {
+            console.log('Vamos a habilitar el campo para el adjunto');
+            otro1.disabled = true;
+        }
+    });
+    // evento para habilitar campo otro2
+    document.getElementById('avance_iteracion').addEventListener('click', function(e) {
+        if (document.formulario.avance_iteracion.selectedIndex == 4) {
+            console.log('Vamos a habilitar el campo para el adjunto');
+            otro2.disabled = false;
+        }
+
+    });
+
+    //evento para desabilitar campo otro2
+    document.getElementById('avance_iteracion').addEventListener('click', function(e) {
+        if (document.formulario.avance_iteracion.selectedIndex != 4) {
+            console.log('Vamos a habilitar el campo para el adjunto');
+            otro2.disabled = true;
+        }
+    });
+
+    document.getElementById('flexRadioDefault2').addEventListener('click', function(e) {
+        console.log('Vamos a deshabilitar el campo para el adjunto');
+        inicio.disabled = true;
+    });
+    document.getElementById('flexRadioDefault1').addEventListener('click', function(e) {
+        console.log('Vamos a deshabilitar el campo para el adjunto');
+        inicio.disabled = false;
+    });
+   
+    $(window).load(function() {
+        $(".loader").fadeOut("slow");
+    });
+    </script>
+    <footer class="bg-light text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ php/generacion.php
@@ -0,0 +1,258 @@
+<?php
+    include("GeneracionArray.php");
+    include("CargueArchivo.php");
+    include("TreeSort.php");
+
+    $generararreglo= new GeneracionArray;
+    $leerArchivo= new cargueArchivo;
+    $ordenamiento_TreeSort = new implementacion();
+
+    $arreglo_tiempos;
+    $arreglo_iteraciones;
+    $arreglo_nombres;
+
+	$algoritmo=$_POST['algoritmo'];
+
+    $tipo_cargue=$_POST['flexRadioDefault'];
+
+    if($tipo_cargue == "archivo"){
+
+
+        $iteraciones = 0;
+
+
+        $files = glob('../archivos/*.txt'); //obtenemos todos los nombres de los ficheros
+        foreach($files as $file){
+            if(is_file($file))
+            unlink($file); //elimino el fichero
+        }        
+
+    //Como el elemento es un arreglos utilizamos foreach para extraer todos los valores
+        foreach($_FILES["adjunto"]['tmp_name'] as $key => $tmp_name)
+        {
+            //Validamos que el archivo exista
+            if($_FILES["adjunto"]["name"][$key]) {
+                $filename = $_FILES["adjunto"]["name"][$key]; //Obtenemos el nombre original del archivo
+                $source = $_FILES["adjunto"]["tmp_name"][$key]; //Obtenemos un nombre temporal del archivo
+                
+                $directorio = '../archivos/'; //Declaramos un  variable con la ruta donde guardaremos los archivos
+                
+                $dir=opendir($directorio); //Abrimos el directorio de destino
+                $target_path = $directorio.'/'.$filename; //Indicamos la ruta de destino, así como el nombre del archivo
+                
+                //Movemos y validamos que el archivo se haya cargado correctamente
+                //El primer campo es el origen y el segundo el destino
+                if(move_uploaded_file($source, $target_path)) {	
+                    $arreglo_nombres[$iteraciones]=$_FILES["adjunto"]["name"][$key];
+                    $iteraciones =  $iteraciones + 1 ;
+                    } else {	
+                    echo "Ha ocurrido un error, por favor inténtelo de nuevo.<br>";
+                }
+                closedir($dir); //Cerramos el directorio de destino
+            }
+        }
+
+    }else{
+        $nombre_archivo="null";
+ 	    $datos_inicio = $_POST['numeros_inicio'];
+        $tipo_dato=$_POST['flexRadioDefault_tipo'];
+        $avance_iteracion = $_POST['avance_iteracion'];
+        $iteraciones=$_POST['Iteraciones'];
+
+        if( $datos_inicio == "Otro"){
+            $datos_inicio=$_POST['otro1'];
+        }
+
+        if( $avance_iteracion == "Otro"){
+            $avance_iteracion=$_POST['otro2'];
+        }  
+    }
+
+
+ 
+    switch ($algoritmo) {
+        case "Tree sort":
+            for ($i = 0; $i < $iteraciones; $i++) {
+                if($tipo_cargue=="archivo"){  
+                     $arreglo=$leerArchivo->leerArchivo($arreglo_nombres[$i]);
+                     $datos_inicio= count($arreglo);
+
+                     $tiempo_inicial = microtime(true);
+                     $ordenamiento_TreeSort-> crearArbol($arreglo);
+                     $tiempo_final = microtime(true);
+                    
+                     $tiempo = $tiempo_final - $tiempo_inicial;
+            
+                     $arreglo_tiempos[$i]=$tiempo;
+                     $arreglo_iteraciones[$i]=$datos_inicio;
+                 }elseif($tipo_cargue=="aleatorios"){
+                     if($tipo_dato=="numerico"){
+                         $arreglo=$generararreglo -> arrayNumerico($datos_inicio);
+                     }elseif($tipo_dato=="letras"){
+                         $arreglo=$generararreglo -> arrayPalabras($datos_inicio);
+                     } 
+
+                     $tiempo_inicial = microtime(true);
+                     $ordenamiento_TreeSort-> crearArbol($arreglo);
+                     $tiempo_final = microtime(true);
+                    
+                     $tiempo = $tiempo_final - $tiempo_inicial;
+            
+                     $arreglo_tiempos[$i]=$tiempo;
+                     $arreglo_iteraciones[$i]=$datos_inicio;
+
+                     $datos_inicio = $datos_inicio + $avance_iteracion ;
+                 }
+             }
+            break;
+        case "Otro1":
+            echo '<script language="javascript">alert("¡El algoritmo no esta disponible!\n");location.href="../index.php";</script>';
+            break;
+        case "Otro2":
+            echo '<script language="javascript">alert("¡El algoritmo no esta disponible!\n");location.href="../index.php";</script>';
+            break;
+        default;
+            echo '<script language="javascript">alert("¡Seleccione un algoritmo!\n");location.href="../index.php";</script>';
+        break;
+    }
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        Proyecto
+    </title>
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <script type="text/javascript" href="../js/bootstrap.min.js"></script>
+    <script type="text/javascript" href="../js/jquery-3.6.1.min.js"></script>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script type="text/javascript">
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(
+            [
+                ['Iteraciones', 'Tiempo'],
+                <?php
+
+
+        for ($i = 0; $i <= count( $arreglo_iteraciones)-1; $i++) {
+            echo('['.$arreglo_iteraciones[$i].', '.$arreglo_tiempos[$i].'],');
+        }
+      
+
+        ?>
+            ]);
+        var options = {
+            hAxis: {
+                title: 'Iteraciones'
+            },
+            vAxis: {
+                title: 'Tiempo'
+            },
+            title: 'Representación temporal Tree Sort - O (n log n)',
+            curveType: 'function',
+            //legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+    }
+    </script>
+</head>
+
+<body>
+    <div class="loading"></div> 
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../index.php">Tree sort</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Inicio</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <h1 id="Generalidades">Tiempo VS Iteración <button type="button" class="btn btn-primary"
+                            onclick="location.href='../index.php'">Atrás</button></h1>
+                    <div id="curve_chart" style="width: 100%; height: 500px"></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <table class="table table-dark table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col"># Iteraciones</th>
+                                <th scope="col">Número de ejecuciones</th>
+                                <th scope="col">Tiempo</th>
+                                <?php
+                                    if($tipo_cargue == "archivo"){
+                                ?>
+                                <th scope="col">Nombre Archivo</th>
+                                <?php
+                                    }
+                                ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                for ($i = 0; $i <= count( $arreglo_iteraciones)-1; $i++) {
+                            ?>
+                            <tr>
+                                <td><?php echo($i+1);?></td>
+                                <td><?php echo($arreglo_iteraciones[$i]);?></td>
+                                <td><?php echo($arreglo_tiempos[$i]);?></td>
+                                <?php
+                                    if($tipo_cargue == "archivo"){
+                                ?>
+                                 <td><?php echo($arreglo_nombres[$i]);?></td>
+                                <?php
+                                    }
+                                ?>
+                            </tr>
+                            <?php
+                                }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div><br>
+    </main>
+
+    <footer class="bg-light text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright
+        </div>
+    </footer>
+
+    <script>
+        $(window).load(function() {
+            $(".loader").fadeOut("slow");
+        });
+    </script>
+</body>
+
+</html>
